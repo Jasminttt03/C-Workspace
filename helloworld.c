a5624105e4e0ef12a3ea966dfa6a2d461b54a400
Thon Than Thar <thonthanthar12122003@gmail.com>
2025-09-04 21:36:46 +0900

    first program

---
+++ helloworld.c
@@ -0,0 +1,6 @@
+#include<stdio.h>  // header file for input/output functions
+
+int main() {
+    printf("Hello, World!\n");  // print message
+    return 0;  // indicate successful program termination
+}
---
+++ main.c
@@ -0,0 +1,7 @@
+#include<stdio.h>  // header file for input/output functions
+int main() {
+
+    main();
+    printf("Hello World.\n");  // print message
+    return 0;  // indicate successful program termination
+}
